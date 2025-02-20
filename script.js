b9236051d2109c9679526b746d3190ebf0c1af9f
kanake <ggeoffreykanake@gmail.com>
2025-02-20 15:43:05 +0300

    Updates

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 $(document).ready(function () {
-  // Toggle menu on click
+  // Menu toggle
   $('#menu').click(function () {
       $(this).toggleClass('fa-times');
       $('header').toggleClass('toggle');
   });
 
-  // Remove menu toggle on scroll or page load
   $(window).on('scroll load', function () {
       $('#menu').removeClass('fa-times');
       $('header').removeClass('toggle');
@@ -14,13 +13,9 @@ $(document).ready(function () {
   // Smooth scrolling for anchor links
   $('a[href*="#"]').on('click', function (e) {
       e.preventDefault();
-      $('html, body').animate(
-          {
-              scrollTop: $($(this).attr('href')).offset().top,
-          },
-          500,
-          'linear'
-      );
+      $('html, body').animate({
+          scrollTop: $($(this).attr('href')).offset().top,
+      }, 500, 'linear');
   });
 
   // Image Expansion (Lightbox Effect)
@@ -28,6 +23,13 @@ $(document).ready(function () {
       var imgSrc = $(this).attr('src'); // Get clicked image source
       $('#modalImg').attr('src', imgSrc); // Set modal image source
       $('#imageModal').fadeIn(); // Show modal
+
+      // Activate panzoom on the modal image
+      $('#modalImg').panzoom({
+          minScale: 1,
+          maxScale: 3, // Allow zooming up to 3x
+          contain: 'invert' // Allow free movement
+      });
   });
 
   // Close Modal on Click (Outside Image)
